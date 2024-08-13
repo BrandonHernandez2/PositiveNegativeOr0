76d5b0f5794dd8ef2cc8f0dba53ac0d7eba9def0
hernabra000 <hernabra000@warren.k12.in.us>
2024-08-13 13:32:05 -0400

    finished the method

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        int numberOne = 5;
+        int numberTwo = -12;
+        int numberThree = 0;
+
+        checkNumber(numberOne);
+        checkNumber(numberTwo);
+        checkNumber(numberThree);
+    }
+
+
+public static void checkNumber(int number){
+        if (number > 0){
+            System.out.println("The number is greater than 0");
+        } else if (number == 0) {
+            System.out.println("The number is equal to 0");
+        } else{
+            System.out.println("The number is less than 0");
+        }
+
+}
 }
